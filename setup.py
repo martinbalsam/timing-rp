6348851727da5d3738545f11fb2e4e24f7450b12
martinbalsam <giuliorasi@gmail.com>
2015-10-06 10:02:33 +0200

    enriched project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='timing-rp',
+    version='0.1dev',
+    packages=['timingrp',],
+    license='GNU General Public License version 2',
+    long_description=open('README.txt').read(),
+)
